d5e5356c2b6c606e7e87ebfae364d3722bc52a78
Maryna Kosse <mkosse@kse.org.ua>
2025-04-08 11:17:12 +0300

    Mul + Div

--- Main.cpp
+++ Main.cpp
@@ -81,4 +81,52 @@ public:
 		vector4 result = vector4(sum[0], sum[1], sum[2], sum[3]);
 		return result;
 	}
+
+	vector4& mul(float scale)
+	{
+		alignas(16) float v1[4] = { this->x(), this->y(), this->z(), this->w() };
+		__m128 v1_simd = _mm_load_ps(v1);
+		__m128 v2_simd = _mm_set_ps(scale, scale, scale, 1);
+		__m128 sum_simd = _mm_mul_ps(v1_simd, v2_simd);
+		alignas(16) float sum[4];
+		_mm_store_ps(sum, sum_simd);
+		vector4 result = vector4(sum[0], sum[1], sum[2], sum[3]);
+		return result;
+	}
+
+	vector4& mul(float scale, float w_scale)
+	{
+		alignas(16) float v1[4] = { this->x(), this->y(), this->z(), this->w() };
+		__m128 v1_simd = _mm_load_ps(v1);
+		__m128 v2_simd = _mm_set_ps(scale, scale, scale, w_scale);
+		__m128 sum_simd = _mm_mul_ps(v1_simd, v2_simd);
+		alignas(16) float sum[4];
+		_mm_store_ps(sum, sum_simd);
+		vector4 result = vector4(sum[0], sum[1], sum[2], sum[3]);
+		return result;
+	}
+
+	vector4& div(float scale)
+	{
+		alignas(16) float v1[4] = { this->x(), this->y(), this->z(), this->w() };
+		__m128 v1_simd = _mm_load_ps(v1);
+		__m128 v2_simd = _mm_set_ps(scale, scale, scale, 1);
+		__m128 sum_simd = _mm_div_ps(v1_simd, v2_simd);
+		alignas(16) float sum[4];
+		_mm_store_ps(sum, sum_simd);
+		vector4 result = vector4(sum[0], sum[1], sum[2], sum[3]);
+		return result;
+	}
+
+	vector4& div(float scale, float w_scale)
+	{
+		alignas(16) float v1[4] = { this->x(), this->y(), this->z(), this->w() };
+		__m128 v1_simd = _mm_load_ps(v1);
+		__m128 v2_simd = _mm_set_ps(scale, scale, scale, w_scale);
+		__m128 sum_simd = _mm_div_ps(v1_simd, v2_simd);
+		alignas(16) float sum[4];
+		_mm_store_ps(sum, sum_simd);
+		vector4 result = vector4(sum[0], sum[1], sum[2], sum[3]);
+		return result;
+	}
 };
