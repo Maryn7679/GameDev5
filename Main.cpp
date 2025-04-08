c0bd09497aae3fb8abb75a009e862d0fd82f6742
Maryna Kosse <mkosse@kse.org.ua>
2025-04-08 11:02:58 +0300

    Add + Sub

--- Main.cpp
+++ Main.cpp
@@ -30,12 +30,52 @@ public:
 	};
 
 
-	vector4& add(const vector4 &other) {
+	vector4& add(const vector4 &other) 
+	{
 		alignas(16) float v1[4] = { this->x(), this->y(), this->z(), this->w() };
 		alignas(16) float v2[4] = { other.x(), other.y(), other.z(), other.w() };
 		__m128 v1_simd = _mm_load_ps(v1);
 		__m128 v2_simd = _mm_load_ps(v2);
 		__m128 sum_simd = _mm_add_ps(v1_simd, v2_simd);
+		alignas(16) float sum[4];
+		_mm_store_ps(sum, sum_simd);
+		vector4 result = vector4(sum[0], sum[1], sum[2], sum[3]);
+		return result;
+	}
+
+	vector4& add(float x, float y, float z) 
+	{
+		alignas(16) float v1[4] = { this->x(), this->y(), this->z(), this->w() };
+		alignas(16) float v2[4] = { x, y, z, 0 };
+		__m128 v1_simd = _mm_load_ps(v1);
+		__m128 v2_simd = _mm_load_ps(v2);
+		__m128 sum_simd = _mm_add_ps(v1_simd, v2_simd);
+		alignas(16) float sum[4];
+		_mm_store_ps(sum, sum_simd);
+		vector4 result = vector4(sum[0], sum[1], sum[2], sum[3]);
+		return result;
+	}
+
+	vector4& sub(const vector4& other) 
+	{
+		alignas(16) float v1[4] = { this->x(), this->y(), this->z(), this->w() };
+		alignas(16) float v2[4] = { other.x(), other.y(), other.z(), other.w() };
+		__m128 v1_simd = _mm_load_ps(v1);
+		__m128 v2_simd = _mm_load_ps(v2);
+		__m128 sum_simd = _mm_sub_ps(v1_simd, v2_simd);
+		alignas(16) float sum[4];
+		_mm_store_ps(sum, sum_simd);
+		vector4 result = vector4(sum[0], sum[1], sum[2], sum[3]);
+		return result;
+	}
+
+	vector4& sub(float x, float y, float z)
+	{
+		alignas(16) float v1[4] = { this->x(), this->y(), this->z(), this->w() };
+		alignas(16) float v2[4] = { x, y, z, 0 };
+		__m128 v1_simd = _mm_load_ps(v1);
+		__m128 v2_simd = _mm_load_ps(v2);
+		__m128 sum_simd = _mm_sub_ps(v1_simd, v2_simd);
 		alignas(16) float sum[4];
 		_mm_store_ps(sum, sum_simd);
 		vector4 result = vector4(sum[0], sum[1], sum[2], sum[3]);
