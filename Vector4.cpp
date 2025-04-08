2a79e21d0178cdda62486540531147ffcb4de205
Maryna Kosse <mkosse@kse.org.ua>
2025-04-08 12:08:02 +0300

    Dot

--- Vector4.cpp
+++ Vector4.cpp
@@ -1,4 +1,5 @@
 #include <xmmintrin.h>
+#include <pmmintrin.h>
 
 struct vector4 
 {
@@ -62,10 +63,10 @@ public:
 		alignas(16) float v2[4] = { other.x(), other.y(), other.z(), other.w() };
 		__m128 v1_simd = _mm_load_ps(v1);
 		__m128 v2_simd = _mm_load_ps(v2);
-		__m128 sum_simd = _mm_sub_ps(v1_simd, v2_simd);
-		alignas(16) float sum[4];
-		_mm_store_ps(sum, sum_simd);
-		vector4 result = vector4(sum[0], sum[1], sum[2], sum[3]);
+		__m128 diff_simd = _mm_sub_ps(v1_simd, v2_simd);
+		alignas(16) float diff[4];
+		_mm_store_ps(diff, diff_simd);
+		vector4 result = vector4(diff[0], diff[1], diff[2], diff[3]);
 		return result;
 	}
 
@@ -75,10 +76,10 @@ public:
 		alignas(16) float v2[4] = { x, y, z, 0 };
 		__m128 v1_simd = _mm_load_ps(v1);
 		__m128 v2_simd = _mm_load_ps(v2);
-		__m128 sum_simd = _mm_sub_ps(v1_simd, v2_simd);
-		alignas(16) float sum[4];
-		_mm_store_ps(sum, sum_simd);
-		vector4 result = vector4(sum[0], sum[1], sum[2], sum[3]);
+		__m128 diff_simd = _mm_sub_ps(v1_simd, v2_simd);
+		alignas(16) float diff[4];
+		_mm_store_ps(diff, diff_simd);
+		vector4 result = vector4(diff[0], diff[1], diff[2], diff[3]);
 		return result;
 	}
 
@@ -87,10 +88,10 @@ public:
 		alignas(16) float v1[4] = { this->x(), this->y(), this->z(), this->w() };
 		__m128 v1_simd = _mm_load_ps(v1);
 		__m128 v2_simd = _mm_set_ps(scale, scale, scale, 1);
-		__m128 sum_simd = _mm_mul_ps(v1_simd, v2_simd);
-		alignas(16) float sum[4];
-		_mm_store_ps(sum, sum_simd);
-		vector4 result = vector4(sum[0], sum[1], sum[2], sum[3]);
+		__m128 prod_simd = _mm_mul_ps(v1_simd, v2_simd);
+		alignas(16) float prod[4];
+		_mm_store_ps(prod, prod_simd);
+		vector4 result = vector4(prod[0], prod[1], prod[2], prod[3]);
 		return result;
 	}
 
@@ -99,10 +100,10 @@ public:
 		alignas(16) float v1[4] = { this->x(), this->y(), this->z(), this->w() };
 		__m128 v1_simd = _mm_load_ps(v1);
 		__m128 v2_simd = _mm_set_ps(scale, scale, scale, w_scale);
-		__m128 sum_simd = _mm_mul_ps(v1_simd, v2_simd);
-		alignas(16) float sum[4];
-		_mm_store_ps(sum, sum_simd);
-		vector4 result = vector4(sum[0], sum[1], sum[2], sum[3]);
+		__m128 prod_simd = _mm_mul_ps(v1_simd, v2_simd);
+		alignas(16) float prod[4];
+		_mm_store_ps(prod, prod_simd);
+		vector4 result = vector4(prod[0], prod[1], prod[2], prod[3]);
 		return result;
 	}
 
@@ -111,10 +112,10 @@ public:
 		alignas(16) float v1[4] = { this->x(), this->y(), this->z(), this->w() };
 		__m128 v1_simd = _mm_load_ps(v1);
 		__m128 v2_simd = _mm_set_ps(scale, scale, scale, 1);
-		__m128 sum_simd = _mm_div_ps(v1_simd, v2_simd);
-		alignas(16) float sum[4];
-		_mm_store_ps(sum, sum_simd);
-		vector4 result = vector4(sum[0], sum[1], sum[2], sum[3]);
+		__m128 res_simd = _mm_div_ps(v1_simd, v2_simd);
+		alignas(16) float res[4];
+		_mm_store_ps(res, res_simd);
+		vector4 result = vector4(res[0], res[1], res[2], res[3]);
 		return result;
 	}
 
@@ -123,10 +124,36 @@ public:
 		alignas(16) float v1[4] = { this->x(), this->y(), this->z(), this->w() };
 		__m128 v1_simd = _mm_load_ps(v1);
 		__m128 v2_simd = _mm_set_ps(scale, scale, scale, w_scale);
-		__m128 sum_simd = _mm_div_ps(v1_simd, v2_simd);
-		alignas(16) float sum[4];
-		_mm_store_ps(sum, sum_simd);
-		vector4 result = vector4(sum[0], sum[1], sum[2], sum[3]);
+		__m128 res_simd = _mm_div_ps(v1_simd, v2_simd);
+		alignas(16) float res[4];
+		_mm_store_ps(res, res_simd);
+		vector4 result = vector4(res[0], res[1], res[2], res[3]);
 		return result;
+	}
+
+	float& dot(const vector4& other)
+	{
+		alignas(16) float v1[4] = { this->x(), this->y(), this->z(), this->w() };
+		alignas(16) float v2[4] = { other.x(), other.y(), other.z(), other.w() };
+		__m128 v1_simd = _mm_load_ps(v1);
+		__m128 v2_simd = _mm_load_ps(v2);
+		__m128 prod_simd = _mm_mul_ps(v1_simd, v2_simd);
+		alignas(16) float prod[4];
+		_mm_store_ps(prod, prod_simd);
+		float dot_product = prod[0] + prod[1] + prod[2] + prod[3];
+		return dot_product;
+	}
+
+	float& dot(float x, float y, float z)
+	{
+		alignas(16) float v1[4] = { this->x(), this->y(), this->z(), this->w() };
+		alignas(16) float v2[4] = { x, y, z, 0 };
+		__m128 v1_simd = _mm_load_ps(v1);
+		__m128 v2_simd = _mm_load_ps(v2);
+		__m128 prod_simd = _mm_mul_ps(v1_simd, v2_simd);
+		alignas(16) float prod[4];
+		_mm_store_ps(prod, prod_simd);
+		float dot_product = prod[0] + prod[1] + prod[2] + prod[3];
+		return dot_product;
 	}
 };
